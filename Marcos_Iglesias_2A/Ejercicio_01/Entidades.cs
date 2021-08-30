42c965bc95368b55794972899c4d7d879b950479
marcosRID <MRID54543@hotmail.com>
2021-08-30 13:49:35 -0300

    1º commit (ejercicio 1 resuelto)

---
+++ Marcos_Iglesias_2A/Ejercicio_01/Entidades.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_01
+{
+    public static class Entidades
+    {
+
+        public static int[] cargarArray(int cantidad)
+        {
+            int[] auxArray = new int[cantidad];
+
+            for (int i = 0; i < auxArray.Length; i++)
+            {
+                while (!int.TryParse(Console.ReadLine(), out auxArray[i]))
+                {
+                    Console.WriteLine("Error en la carga, \"Solo numeros prositivos enteros\"");
+                }
+            }
+
+            return auxArray;
+        }
+
+        public static int cacularMayor(int[] auxArray)
+        {
+            int numMax = 0 ;
+            bool flag = true;
+
+            foreach (int i in auxArray)
+            {
+                if (flag)
+                {
+                    numMax = i;
+                    flag = false;
+                }
+                else
+                {    
+                    if (i == 0 || i > numMax)
+                    {
+                        numMax = i;
+                    }
+                }
+
+            }
+
+            return numMax;
+        }
+        
+        public static int cacularMenor(int[] auxArray)
+        {
+            int numMin = 0;
+            bool flag = true;
+
+            foreach (int i in auxArray)
+            {
+                if (flag)
+                {
+                    numMin = i;
+                    flag = false;
+                }
+                else
+                {
+                    if (i == 0 || i < numMin)
+                    {
+                        numMin = i;
+                    }
+                }
+            }
+
+            return numMin;
+        }
+
+        public static double carlularPromedio(int[] auxArray)
+        {
+            int cantidad = 0;
+            int suma = 0;
+            
+
+            foreach (int i in auxArray)
+            {
+                suma += auxArray[cantidad];
+                cantidad++;
+            }
+
+            return (double) suma / cantidad;
+        }
+
+    }
+}
---
+++ Marcos_Iglesias_2A/Ejercicio_01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ejercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 
+
+            Console.Title = "01";
+            #endregion
+
+            int cantidad = 5;
+            int[] array_numeros = new int[cantidad];
+
+            int numMax = 0;
+            int numMin = 0;
+            double promedio;
+
+            Console.WriteLine("Ingrese 5 numeros");
+
+            array_numeros = Entidades.cargarArray(cantidad);
+
+            numMax = Entidades.cacularMayor(array_numeros);
+            numMin = Entidades.cacularMenor(array_numeros);
+            promedio = Entidades.carlularPromedio(array_numeros);
+
+            Console.WriteLine($" Numero maximo : {numMax}\n Numero minimo : {numMin} \n Promedio : {promedio}");
+
+        }
+    }
+}
